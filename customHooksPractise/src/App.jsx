203a452b6555bdf4e0ca8fc468e214309b304515
artypratham <prathameshdharmadhikari3@gmail.com>
2024-04-27 10:56:12 +0530

    custom hook1

--- customHooksPractise/src/App.jsx
+++ customHooksPractise/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
-import './App.css'
+import './App.css';
+import axios from "axios";
+// function App() {
 
-function App() {
-
-  return (
-    <>
-      <MyComponent/>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <MyComponent/>
+//     </>
+//   )
+// }
 
 
 //function based components using hooks
@@ -52,30 +52,116 @@ function App() {
 
 
 
+//a function to make the component disappear after 5 seconds
+// function MyComponent() {
 
-function MyComponent() {
+//   const [isVisible,setIsVisible] = useState(true);
 
-  const [isVisible,setIsVisible] = useState(true);
 
+//   useEffect(() => {
+//     const timer = setTimeout(()=> {
+//       setIsVisible(false)
+//     },5000)
+
+//     return () => clearTimeout(timer);
+    
+//   }, []);
+
+    
+//   return isVisible? (
+//     <div>
+//     From inside my component 
+//     </div> 
+//   ) : null;
+// }
+
+
+
+
+//typical way without custom hooks
+
+// function App() {
+  
+//   const [todos,setTodos] = useState([])
+
+//   useEffect(()=>{
+//     axios.get('https://sum-server.100xdevs.com/todos')
+//     .then(res => {
+//       setTodos(res.data.todos);
+//     })
+//   },[])
+  
+  
+//   return(
+//   <>
+//     {todos.map(todo => <Track  todo={todo} key={todo.id}/>)}  
+//   </>
+//   )
+// }
+
+
+
+
+
+// function Track ({todo}) {
+
+//   return(
+//     <>
+//       <div>
+//         {todo.title}  
+//       </div>  
+//       <br/>
+//       <div>
+//         {todo.description}
+//       </div>
+//     </>
+//   )
+
+// }
+
+
+function App() {
+  const todos = useTodos();
+  return(
+    <>
+      {todos.map(todo => <Track todo={todo} key={todo.id} />)}
+    </>
+  )
+}
+
+
+function useTodos() {
+  const [todos, setTodos]  =useState([])
 
   useEffect(() => {
-    const timer = setTimeout(()=> {
-      setIsVisible(false)
-    },5000)
+    axios.get("https://sum-server.100xdevs.com/todos")
+    .then(res=> {
+      setTodos(res.data.todos)
+    })
+  },[])
 
-    return () => clearTimeout(timer);
-    
-  }, []);
+  return todos;
+}
 
-    
-  return isVisible? (
+
+function Track({todo}) {
+  return(
+    <>
     <div>
-    From inside my component 
-    </div> 
-  ) : null;
+     {todo.title}
+   </div>
+   <div>
+     {todo.description}
+   </div>
+   </>
+  )
+
+
+  
 }
 
 
 
-export default App
+
+export default App;
  
