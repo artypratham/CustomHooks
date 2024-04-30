a34e4d9a4ab84ecbbfb6513925a9174108b732c7
artypratham <prathameshdharmadhikari3@gmail.com>
2024-04-30 09:49:18 +0530

    created a useMousePointer custom hook

--- customHooksPractise/src/App.jsx
+++ customHooksPractise/src/App.jsx
@@ -242,18 +242,38 @@ import axios from "axios";
 
 
 
+//complete useMousePointer hook 
+
+function useMousePointer() {
+  const [ position, setPosition] = useState({x: 0, y: 0});
+
+  const handleMouseMove = (e) => {
+    setPosition({x: e.clientX, y: e.clientY});
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return() => {
+      window.removeEventListener('mousemove', handleMouseMove)
+    };
+  }, []);
+
+  return position;
+}
+
+function App(){
+  const mousePointer = useMousePointer()
+
+  return (
+    <>
+    Your mouse pointer is {mousePointer.x} {mousePointer.y}
+    </>
+  )
+
+}
 
 
-
-
-
-
-
-
-
-
-
-
+export default App
 
 
 
